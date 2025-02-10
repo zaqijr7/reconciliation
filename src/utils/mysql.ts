64ecda0e74795d5ed1236539f4431b98905414b5
muhammad.zaqi <muhammadzaqi.quraisyi@mandalafinance.com>
2025-02-10 23:39:46 +0700

    push env:

--- src/utils/mysql.ts
+++ src/utils/mysql.ts
@@ -1,10 +1,11 @@
 import mysql from "mysql2";
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "recon",
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT ? parseInt(process.env.MYSQL_PORT) : 0,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 // Connect to the database
